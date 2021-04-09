a95256ec79e5b2a1fe48eac769e005b07ed2e7a1
Bruno Santos <brunodosantos@gmail.com>
2021-04-09 08:37:03 -0300

    changing outputs function

--- R/outputs.R
+++ R/outputs.R
@@ -1,7 +1,21 @@
 outputs <- function(chain) {
-  out <- cbind(t(summary(chain)),
+  out <- as.data.frame(cbind(t(summary(chain)),
                as.matrix(stats::sd(chain)),
                t(stats::quantile(chain, probs = c(0.025, 0.975))),
-               t(TeachingDemos::emp.hpd(chain, conf = 0.95)))
-  return(out)
+               t(TeachingDemos::emp.hpd(chain, conf = 0.95))))
+  colnames(out) <-
+    c(
+      "min",
+      "1qt",
+      "med",
+      "media",
+      "3qt",
+      "max",
+      "sd",
+      "emp_025",
+      "emp_975",
+      "HPD_025",
+      "HPD_975"
+    )
+  out
 }
