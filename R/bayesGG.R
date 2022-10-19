00ebb56a24143cf7811d54302df3ef9e371fb2b4
Bruno Santos <brunodosantos@gmail.com>
2022-10-19 17:58:04 +0100

    Changing package to allow covariates for all parameters

--- R/bayesGG.R
+++ R/bayesGG.R
@@ -2,10 +2,14 @@
 #'
 #' This function returns the fit for a Bayesian regression model.
 #'
-#' @param linear_pred the linear predictor formula to be included in the model,
-#'  in the LHS one put the name of variable to considered as the response
-#'  variable and in the RHS one puts the name of explanatory variables,
-#'  where all variables are included in data.
+#' @param linear_pred_mu the linear predictor formula to be included in the 
+#'  model for the conditional quantiles; in the LHS one put the name of 
+#'  variable to considered as the response variable and in the RHS one puts the
+#'  name of explanatory variables, where all variables are included in data.
+#' @param linear_pred_alpha the formula for the linear predictor of alpha. 
+#' There is no need to put the LHs of the formula
+#' @param linear_pred_lambda the formula for the linear predictor of alpha. 
+#' There is no need to put the LHs of the formula    
 #' @param data data to be calculated the quantile of interest.
 #' @param q quantile of interest.
 #' @param d name of variable containing the censoring indicator in data.
@@ -18,57 +22,63 @@
 #' @import LaplacesDemon
 #' @import truncnorm
 
-bayesGG  <- function(linear_pred, data, q, d, iter = 1000, burn, jump, guess) {
+bayesGG  <- function(linear_pred_mu,
+                     linear_pred_alpha,
+                     linear_pred_lambda,
+                     data, q, d, iter = 1000, 
+                     burn, jump, guess = NULL) {
   mon.names <- c("LP")
-  J <- dim(stats::model.matrix(linear_pred, data))[2] - 1
+  J1 <- dim(stats::model.matrix(linear_pred_mu, data))[2] 
+  J2 <- dim(stats::model.matrix(linear_pred_alpha, data))[2] 
+  J3 <- dim(stats::model.matrix(linear_pred_lambda, data))[2] 
   parm.names <- LaplacesDemon::as.parm.names(list(
-    alpha = 0,
-    lambda = 0,
-    beta = rep(0, (J + 1))
+    beta = rep(0, J1),
+    gama = rep(0, J2), 
+    vi = rep(0, J3)
   ))
-  pos.alpha  <- grep("alpha", parm.names)
-  pos.lambda   <- grep("lambda", parm.names)
   pos.beta <- grep("beta", parm.names)
-
+  pos.gama <- grep("gama", parm.names)
+  pos.vi <- grep("vi", parm.names)
+  
+  if (is.null(guess)) guess <- rep(0, J1 + J2 + J3)
+  
   MyData <- list(
-    linear_pred = linear_pred,
+    linear_pred_mu = linear_pred_mu,
+    linear_pred_alpha = linear_pred_alpha,
+    linear_pred_lambda = linear_pred_lambda,
     data = data,
-    J = J,
-    q = q,
     d = d,
+    q = q,
     log = TRUE,
     mon.names = mon.names,
-    parm.names = parm.names,
-    pos.alpha = pos.alpha,
-    pos.lambda = pos.lambda,
+    parm.names = parm.names, 
     pos.beta = pos.beta,
-    N = 1
+    pos.gama = pos.gama,
+    pos.vi = pos.vi,
+    N = 1  
   )
 
   Model <- function(parm, Data) {
     ### Parameters
-    alpha  <- LaplacesDemon::interval(parm[Data$pos.alpha], -Inf, -1e-100)
-    parm[Data$pos.alpha]  <- alpha
-    lambda  <-  LaplacesDemon::interval(parm[Data$pos.lambda], 1e-100, Inf)
-    parm[Data$pos.lambda] <- lambda
     beta <-  parm[Data$pos.beta]
+    gama <-  parm[Data$pos.gama]
+    vi <-  parm[Data$pos.vi]
 
     ### Log(Prior Densities)
-    alpha.prior <-
-      log(truncnorm::dtruncnorm(
-        alpha,
-        a = -Inf,
-        b = -1e-100,
-        mean = -0.1,
-        sd = 10
-      ))
-    lambda.prior <-
-      stats::dgamma(lambda,
-             shape = 0.01,
-             rate = 0.01,
-             log = TRUE) #mean 1 and var 100
-    beta.prior <- sum(LaplacesDemon::dnormv(
+    beta.prior <- sum(dnormv(
       beta,
+      mean = 0,
+      var = 100,
+      log = TRUE
+    ))
+    gama.prior <- sum(dnormv(
+      gama,
+      mean = 0,
+      var = 100,
+      log = TRUE
+    ))
+    vi.prior <- sum(dnormv(
+      vi,
       mean = 0,
       var = 100,
       log = TRUE
@@ -77,17 +87,18 @@ bayesGG  <- function(linear_pred, data, q, d, iter = 1000, burn, jump, guess) {
     ### Log-Likelihood
     LL <-
       likGG(
-        par = c(alpha, lambda, beta),
+        par = c(beta, gama, vi),
         q = Data$q,
         d = Data$d,
         data = Data$data,
-        linear_pred = Data$linear_pred,
-        J = Data$J,
+        linear_pred_mu = Data$linear_pred_mu,
+        linear_pred_alpha = Data$linear_pred_alpha,
+        linear_pred_lambda = Data$linear_pred_lambda,
         log = Data$log
       )
 
     ### Log-Posterior
-    LP <- LL + alpha.prior + lambda.prior + beta.prior
+    LP <- LL + beta.prior + gama.prior + vi.prior
     Modelout <-
       list(
         LP = LP,
--- R/likGG.R
+++ R/likGG.R
@@ -9,28 +9,49 @@
 #'  column with d value containing a censoring indicator, where 1 corresponds
 #'  to a non-censored observation and 0 otherwise. It must also contain a x
 #'  column with
-#' @param linear_pred the linear predictor formula to be included in the model,
-#'  in the LHS one put the name of variable to considered as the response
-#'  variable and in the RHS one puts the name of explanatory variables,
-#'  where all variables are included in data.
-#' @param J number of regressors in the model.
+#' @param linear_pred_mu the linear predictor formula to be included in the 
+#'  model for the conditional quantiles; in the LHS one put the name of 
+#'  variable to considered as the response variable and in the RHS one puts the
+#'  name of explanatory variables, where all variables are included in data.
+#' @param linear_pred_alpha the formula for the linear predictor of alpha. 
+#' There is no need to put the LHs of the formula
+#' @param linear_pred_lambda the formula for the linear predictor of alpha. 
+#' There is no need to put the LHs of the formula    
 #' @param d name of variable containing the censoring indicator in data.
 #' @param log if the log-likelihood is to be calculated or not. Default is
 #'  FALSE.
 #' @return the likelihood function value given the provided information.
 #' @export
 
-likGG <- function(par, q, data, linear_pred, J, d, log = FALSE) {
-  alpha <- par[1]
-  lambda <- par[2]
-  beta <- par[3:(3 + J)]
-
+likGG <- function(par, q, data, 
+                  linear_pred_mu,
+                  linear_pred_alpha, 
+                  linear_pred_lambda,
+                   d, log = FALSE) {
+  
+  J1 <- dim(stats::model.matrix(linear_pred_mu, data))[2] 
+  J2 <- dim(stats::model.matrix(linear_pred_alpha, data))[2]
+  J3 <- dim(stats::model.matrix(linear_pred_lambda, data))[2] 
+  
+  beta <- par[1:J1]
+  gama <- par[(1 + J1):(J1 + J2)]
+  vi <- par[(J1 + J2 + 1):(J1 + J2 + J3)]
+  
   cens <- get(d, data)
   t <- as.numeric(
-    stats::model.extract(stats::model.frame(linear_pred, data), 'response')
+    stats::model.extract(stats::model.frame(linear_pred_mu, data), 
+                         'response')
   )
-  X <- stats::model.matrix(linear_pred, data)
+  X <- stats::model.matrix(linear_pred_mu, data)
   mu <- exp(tcrossprod(X, t(beta)))
+  
+  X2 <- stats::model.matrix(linear_pred_alpha, data)
+  alpha <- -exp(tcrossprod(X2, t(gama)))
+  
+  X3 <- stats::model.matrix(linear_pred_lambda, data)
+  lambda <- exp(tcrossprod(X3, t(vi)))
+  
+  
   out <-
     sum(
       cens * dGG(
